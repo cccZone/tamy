b9f31a1efbc5778221b96cff9eb3d108849d3d7c
ptrochim@gmail.com <ptrochim@gmail.com@27bfb670-012f-11de-85c2-bb0660c75f22>
2012-06-15 22:40:57 +0000

    unit test bug fix

--- trunk/Code/Tests/test-core/PlatformTests.cpp
+++ trunk/Code/Tests/test-core/PlatformTests.cpp
@@ -6,8 +6,8 @@
 
 TEST( Types, typesSizeCheck )
 {
-   CPPUNIT_ASSERT_EQUAL( 1, sizeof( byte ) );
-   CPPUNIT_ASSERT_EQUAL( 2, sizeof( word ) );   // if this type's not 2 bytes long, index buffers allocated with format D3DFMT_INDEX16 will not work the way we currently handle them 
-   CPPUNIT_ASSERT_EQUAL( 4, sizeof( uint ) );
-   CPPUNIT_ASSERT_EQUAL( 8, sizeof( ulong ) );
+   CPPUNIT_ASSERT_EQUAL( (uint)1, sizeof( byte ) );
+   CPPUNIT_ASSERT_EQUAL( (uint)2, sizeof( word ) );   // if this type's not 2 bytes long, index buffers allocated with format D3DFMT_INDEX16 will not work the way we currently handle them 
+   CPPUNIT_ASSERT_EQUAL( (uint)4, sizeof( uint ) );
+   CPPUNIT_ASSERT_EQUAL( (uint)4, sizeof( ulong ) );
 }
